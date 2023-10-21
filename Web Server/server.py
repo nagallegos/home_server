ab1a27ceb9c05a0dfe5e2e8f029517fa606520dd
nagallegos <113269909+nagallegos@users.noreply.github.com>
2023-10-21 15:46:10 -0500

    Added page for controller and separated from index

--- Web Server/server.py
+++ Web Server/server.py
@@ -12,39 +12,43 @@ led1,led2,led3= 11,13,15
 def index():
     return render_template('index.html')
 
+@app.route('/RPi_control')
+def RPi_control():
+    return render_template('RPi_control.html')
+
 @app.route('/A')
 def led1on():
     #rpi.output(led1,1)
     blink.blink1(led1)
-    return render_template('index.html')
+    return render_template('RPi_control.html')
 
 """
 @app.route('/a')
 def led1off():
     rpi.output(led1,0)
-    return render_template('index.html')
+    return render_template('RPi_control.html')
 """
 @app.route('/B')
 def led2on():
     #rpi.output(led2,1)
     blink.blink1(led2)
-    return render_template('index.html')
+    return render_template('RPi_control.html')
 """
 @app.route('/b')
 def led2off():
     rpi.output(led2,0)
-    return render_template('index.html')
+    return render_template('RPi_control.html')
 """
 @app.route('/C')
 def led3on():
     #rpi.output(led3,1)
     blink.blink1(led3)
-    return render_template('index.html')
+    return render_template('RPi_control.html')
 """
 @app.route('/c')
 def led3off():
     rpi.output(led3,0)
-    return render_template('index.html')
+    return render_template('RPi_control.html')
 """
 if __name__=="__main__":
     print("Start")
